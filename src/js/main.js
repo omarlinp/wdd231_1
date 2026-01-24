89fde8431fb74ffab647c5e48000012c7224c79e
omarlinp <omarlinp@gmail.com>
2026-01-24 09:52:35 -0400

    completed NPS 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
