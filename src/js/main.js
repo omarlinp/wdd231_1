c293382b0fc0b66611a950287333dcafd636850d
omarlinp <omarlinp@gmail.com>
2026-02-10 19:26:35 -0400

    completed the struturing and styling of the NPS project part 2

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,79 @@ function setHeaderInfo(data) {
   document.querySelector(".hero-banner__content").innerHTML =
     parkInfoTemplate(data);
 }
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${data.fullName}</h1><p>${data.description}</p>`;
+}
+function setParkInfoLinks(data) {
+const infoEl = document.querySelector(".info");
+const html = data.map(mediaCardTemplate);
+infoEl.innerHTML = html.join("");
+
+}
+function setFooter(data){
+  const footerEl = document.querySelector("#park-footer");
+  footerEl.innerHTML = footerTemplate(data);
+}
+function mediaCardTemplate(info) {
+  return `<a href="${info.link}" class="media-card">
+    <img src="${info.image}" alt="${info.name}" class="media-card__image" />
+    <div class="media-card__content">
+      <h3 class="media-card__title">${info.name}</h3>
+      <p class="media-card__description">${info.description}</p>
+    </div>
+  </a>`;}
+
+function getMailingAddress(addresses) {
+  const mailing = addresses.find((address)=> address.type === "Mailing");
+  return mailing;
+}
+function getVoicePhone(addresses) {
+  const voice = addresses.find((address)=> address.type === "Voice");
+  return voice;
+  
+}
+function footerTemplate(info) {
+  const mailing = getMailingAddress(info.addresses);
+  const voice = getVoicePhone(info.contacts.phoneNumbers);
+  return `
+  <section class="contact">
+    <h3>Contact Info</h3>
+    <h4>Mailing Address</h4>
+    <div>
+    <p>${mailing.line1}</p>
+    <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p>
+    <h4>Phone</h4>
+    <p>${voice}</p> 
+    </div>
+  </section>
+  `;
+}
+
+const parkInfoLinks = [
+  {
+    name: "Current Conditions &#x203A;",
+    link: "conditions.html",
+    image: parkData.images[2].url,
+    description:
+      "See what conditions to expect in the park before leaving on your trip!"
+  },
+  {
+    name: "Fees and Passes &#x203A;",
+    link: "fees.html",
+    image: parkData.images[3].url,
+    description: "Learn about the fees and passes that are available."
+  },
+  {
+    name: "Visitor Centers &#x203A;",
+    link: "visitor_centers.html",
+    image: parkData.images[9].url,
+    description: "Learn about the visitor centers in the park."
+  }
+];
+
+
+setHeaderInfo(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
+setFooter(parkData);
